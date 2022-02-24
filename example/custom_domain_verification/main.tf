1b8b4cd1f32b740e9db3dc07b0cd6c5785dd15e0
Sergiu Ghitea <sergiu.ghitea@auth0.com>
2022-02-24 14:44:04 +0100

    Add custom_domain_verification example

---
+++ example/custom_domain_verification/main.tf
@@ -0,0 +1,19 @@
+provider "auth0" {}
+
+resource "auth0_custom_domain" "my_custom_domain" {
+  domain = "login.example.com"
+  type   = "auth0_managed_certs"
+}
+
+resource "auth0_custom_domain_verification" "my_custom_domain_verification" {
+  custom_domain_id = auth0_custom_domain.my_custom_domain.id
+  timeouts { create = "15m" }
+  depends_on = [digitalocean_record.my_domain_name_record]
+}
+
+resource "digitalocean_record" "my_domain_name_record" {
+  domain = "example.com"
+  type   = upper(auth0_custom_domain.my_custom_domain.verification[0].methods[0].name)
+  name   = "${auth0_custom_domain.my_custom_domain.domain}."
+  value  = "${auth0_custom_domain.my_custom_domain.verification[0].methods[0].record}."
+}

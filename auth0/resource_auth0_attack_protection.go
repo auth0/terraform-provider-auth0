ed3a43da0ca92ab01116089eb66a4744939aea58
Will Vedder <willvedder@jxmqxvcv5v.myfiosgateway.com>
2022-03-01 17:53:06 -0500

    Removing commented-out code

--- auth0/resource_auth0_attack_protection.go
+++ auth0/resource_auth0_attack_protection.go
@@ -345,15 +345,15 @@ func expandSuspiciousIPThrottling(d *schema.ResourceData) *management.Suspicious
 			shields = append(shields, fmt.Sprintf("%s", s))
 		}
 
-		// var allowlist []string
-		// for _, a := range d.Get("allowlist").([]interface{}) {
-		// 	allowlist = append(allowlist, fmt.Sprintf("%s", a))
-		// }
+		allowlist := []string{}
+		for _, a := range d.Get("allowlist").([]interface{}) {
+			allowlist = append(allowlist, fmt.Sprintf("%s", a))
+		}
 
 		ipt = &management.SuspiciousIPThrottling{
-			Enabled: Bool(d, "enabled"),
-			Shields: &shields,
-			//AllowList: &allowlist,
+			Enabled:   Bool(d, "enabled"),
+			Shields:   &shields,
+			AllowList: &allowlist,
 			Stage: &management.Stage{
 				PreUserRegistration: &management.PreUserRegistration{},
 				PreLogin:            &management.PreLogin{},
@@ -383,15 +383,15 @@ func expandBruteForceProtection(d *schema.ResourceData) *management.BruteForcePr
 			shields = append(shields, fmt.Sprintf("%s", s))
 		}
 
-		// var allowlist []string
-		// for _, a := range d.Get("allowlist").([]interface{}) {
-		// 	allowlist = append(allowlist, fmt.Sprintf("%s", a))
-		// }
+		allowlist := []string{}
+		for _, a := range d.Get("allowlist").([]interface{}) {
+			allowlist = append(allowlist, fmt.Sprintf("%s", a))
+		}
 
 		bfp = &management.BruteForceProtection{
-			Enabled: Bool(d, "enabled"),
-			Shields: &shields,
-			//AllowList:   &allowlist,
+			Enabled:     Bool(d, "enabled"),
+			Shields:     &shields,
+			AllowList:   &allowlist,
 			Mode:        String(d, "mode"),
 			MaxAttempts: Int(d, "max_attempts"),
 		}

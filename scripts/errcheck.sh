72a32751f1843f7d169fcbbbce121de97e5d36bd
Sergiu Ghitea <sergiu.ghitea@auth0.com>
2022-03-25 14:35:12 +0100

    Refactor errcheck script

--- scripts/errcheck.sh
+++ scripts/errcheck.sh
@@ -1,23 +1,23 @@
 #!/usr/bin/env bash
 
-# Check gofmt
-echo "==> Checking for unchecked errors..."
-
 if ! which errcheck > /dev/null; then
     echo "==> Installing errcheck..."
-    go get -u github.com/kisielk/errcheck
+    go install github.com/kisielk/errcheck@latest
 fi
 
-err_files=$(errcheck -ignoretests \
-                     -ignore 'github.com/hashicorp/terraform-plugin-sdk/helper/schema:Set' \
-                     -ignore 'bytes:.*' \
-                     -ignore 'io:Close|Write' \
-                     $(go list ./...| grep -v /vendor/))
+err_files=$(
+    # shellcheck disable=SC2046
+    errcheck \
+        -ignoretests \
+        -ignore 'bytes:.*' \
+        -ignore 'io:Close|Write' \
+    $(go list ./...| grep -v /vendor/) \
+)
 
 if [[ -n ${err_files} ]]; then
-    echo 'Unchecked errors found in the following places:'
+    echo "Unchecked errors found in the following places:"
     echo "${err_files}"
-    echo "Please handle returned errors. You can check directly with \`make errcheck\`"
+    echo "Please handle returned errors."
     exit 1
 fi
 

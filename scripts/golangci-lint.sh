cad7aeb2a47a411f302654817cf5888ee027ee41
Sergiu Ghitea <sergiu.ghitea@auth0.com>
2022-04-06 13:04:18 +0200

    Add pre-commit config

---
+++ scripts/golangci-lint.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+if ! command -v golangci-lint &> /dev/null ; then
+    echo "golangci-lint not installed or available in the PATH" >&2
+    echo "please check https://github.com/golangci/golangci-lint" >&2
+    exit 1
+fi
+
+exec golangci-lint run -c .golangci.yml --allow-parallel-runners --fix ./...

5f471a67ed43a95c25986f9ed614bc2a722e9cd3
Noah Fontes <noah.fontes@puppet.com>
2022-04-10 01:58:02 -0700

    Add tests for custom domains with self-managed certificates

--- auth0/resource_auth0_custom_domain_verification_test.go
+++ auth0/resource_auth0_custom_domain_verification_test.go
@@ -7,7 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 )
 
-func TestAccCustomDomainVerification(t *testing.T) {
+func TestAccCustomDomainVerificationWithAuth0ManagedCerts(t *testing.T) {
 	resource.Test(t, resource.TestCase{
 		ProviderFactories: map[string]func() (*schema.Provider, error){
 			"auth0": func() (*schema.Provider, error) {
@@ -16,21 +16,94 @@ func TestAccCustomDomainVerification(t *testing.T) {
 		},
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCustomDomainVerification,
+				Config: testAccCustomDomainVerificationWithAuth0ManagedCerts,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("auth0_custom_domain.my_custom_domain", "domain", "terraform-provider.auth0.com"),
 					resource.TestCheckResourceAttr("auth0_custom_domain.my_custom_domain", "type", "auth0_managed_certs"),
-					resource.TestCheckResourceAttrSet("auth0_custom_domain_verification.my_custom_domain_verification", "custom_domain_id"),
+					// The status attribute is set to "pending_verification"
+					// here because Terraform has settled its state before
+					// attempting the custom domain verification. We need to
+					// refresh the state to move it along.
+					resource.TestCheckResourceAttr("auth0_custom_domain.my_custom_domain", "status", "pending_verification"),
+					resource.TestCheckResourceAttrPair(
+						"auth0_custom_domain.my_custom_domain", "id",
+						"auth0_custom_domain_verification.my_custom_domain_verification", "custom_domain_id",
+					),
+					resource.TestCheckResourceAttrSet("auth0_custom_domain_verification.my_custom_domain_verification", "origin_domain_name"),
+				),
+			},
+			{
+				Config: testAccCustomDomainVerificationWithAuth0ManagedCerts,
+				Check: resource.ComposeTestCheckFunc(
+					// By applying an identical plan, we can reconcile the
+					// status attribute.
+					resource.TestCheckResourceAttr("auth0_custom_domain.my_custom_domain", "status", "ready"),
+					resource.TestCheckResourceAttrPair(
+						"auth0_custom_domain.my_custom_domain", "origin_domain_name",
+						"auth0_custom_domain_verification.my_custom_domain_verification", "origin_domain_name",
+					),
 				),
 			},
 		},
 	})
 }
 
-const testAccCustomDomainVerification = `
+const testAccCustomDomainVerificationWithAuth0ManagedCerts = `
 resource "auth0_custom_domain" "my_custom_domain" {
 	domain = "terraform-provider.auth0.com"
 	type = "auth0_managed_certs"
+}
+
+resource "auth0_custom_domain_verification" "my_custom_domain_verification" {
+	custom_domain_id = auth0_custom_domain.my_custom_domain.id
+	timeouts { create = "15m" }
+}
+`
+
+func TestAccCustomDomainVerificationWithSelfManagedCerts(t *testing.T) {
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: map[string]func() (*schema.Provider, error){
+			"auth0": func() (*schema.Provider, error) {
+				return providerWithMockedAPI(), nil
+			},
+		},
+		Steps: []resource.TestStep{
+			{
+				Config: testAccCustomDomainVerificationWithSelfManagedCerts,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_custom_domain.my_custom_domain", "domain", "terraform-provider.auth0.com"),
+					resource.TestCheckResourceAttr("auth0_custom_domain.my_custom_domain", "type", "self_managed_certs"),
+					resource.TestCheckResourceAttr("auth0_custom_domain.my_custom_domain", "status", "pending_verification"),
+					resource.TestCheckResourceAttrPair(
+						"auth0_custom_domain.my_custom_domain", "id",
+						"auth0_custom_domain_verification.my_custom_domain_verification", "custom_domain_id",
+					),
+					resource.TestCheckResourceAttrSet("auth0_custom_domain_verification.my_custom_domain_verification", "origin_domain_name"),
+					resource.TestCheckResourceAttrSet("auth0_custom_domain_verification.my_custom_domain_verification", "cname_api_key"),
+				),
+			},
+			{
+				Config: testAccCustomDomainVerificationWithSelfManagedCerts,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_custom_domain.my_custom_domain", "status", "ready"),
+					resource.TestCheckResourceAttrPair(
+						"auth0_custom_domain.my_custom_domain", "origin_domain_name",
+						"auth0_custom_domain_verification.my_custom_domain_verification", "origin_domain_name",
+					),
+					// Even though we can no longer read this from the API, it
+					// should remain set after refresh as we won't clear it out
+					// in the read operation.
+					resource.TestCheckResourceAttrSet("auth0_custom_domain_verification.my_custom_domain_verification", "cname_api_key"),
+				),
+			},
+		},
+	})
+}
+
+const testAccCustomDomainVerificationWithSelfManagedCerts = `
+resource "auth0_custom_domain" "my_custom_domain" {
+	domain = "terraform-provider.auth0.com"
+	type = "self_managed_certs"
 }
 
 resource "auth0_custom_domain_verification" "my_custom_domain_verification" {

99d1986992d6c36ac457ce17007fb30c88528af2
Will Vedder <willvedder@jxmqxvcv5v.myfiosgateway.com>
2022-07-20 08:43:21 -0400

    Adding auth0_organization_member resource along with tests

---
+++ auth0/resource_auth0_organization_member.go
@@ -0,0 +1,142 @@
+package auth0
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/auth0/go-auth0/management"
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+)
+
+func newOrganizationMember() *schema.Resource {
+	return &schema.Resource{
+		CreateContext: createOrganizationMember,
+		ReadContext:   readOrganizationMember,
+		UpdateContext: updateOrganizationMember,
+		DeleteContext: deleteOrganizationMember,
+		Importer: &schema.ResourceImporter{
+			StateContext: schema.ImportStatePassthroughContext,
+		},
+		Schema: map[string]*schema.Schema{
+			"organization_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				Description: "The ID of the organization to assign member to",
+			},
+			"user_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "ID of the user to add as organization member",
+			},
+			"roles": {
+				Type:        schema.TypeSet,
+				Elem:        &schema.Schema{Type: schema.TypeString},
+				Description: "Role ID(s) to assign to member",
+				Optional:    true,
+			},
+		},
+	}
+}
+
+func createOrganizationMember(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	userID := String(d, "user_id")
+	orgID := String(d, "organization_id")
+
+	d.SetId(*userID)
+
+	api := m.(*management.Management)
+	if err := api.Organization.AddMembers(*orgID, []string{*userID}); err != nil {
+		return diag.FromErr(err)
+	}
+
+	if err := assignRoles(d, m); err != nil {
+		return diag.FromErr(fmt.Errorf("failed to assign roles to organization member. %w", err))
+	}
+
+	return readOrganizationMember(ctx, d, m)
+}
+
+func assignRoles(d *schema.ResourceData, m interface{}) error {
+	api := m.(*management.Management)
+	orgID := String(d, "organization_id")
+	memberID := String(d, "user_id")
+
+	add, rm := Diff(d, "roles")
+
+	rolesToAssign := []string{}
+	for _, r := range add.List() {
+		rolesToAssign = append(rolesToAssign, r.(string))
+	}
+	if len(rolesToAssign) > 0 {
+		err := api.Organization.AssignMemberRoles(*orgID, *memberID, rolesToAssign)
+		if err != nil {
+			return err
+		}
+	}
+
+	rolesToRemove := []string{}
+	for _, r := range rm.List() {
+		rolesToRemove = append(rolesToRemove, r.(string))
+	}
+	if len(rolesToRemove) > 0 {
+		err := api.Organization.DeleteMemberRoles(*orgID, *memberID, rolesToRemove)
+		if err != nil {
+			return err
+		}
+	}
+
+	return nil
+}
+
+func readOrganizationMember(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	api := m.(*management.Management)
+
+	orgID := String(d, "organization_id")
+	memberID := String(d, "user_id")
+
+	roles, err := api.Organization.MemberRoles(*orgID, *memberID)
+	if err != nil {
+		if mErr, ok := err.(management.Error); ok {
+			if mErr.Status() == http.StatusNotFound {
+				d.SetId("")
+				return nil
+			}
+		}
+		return diag.FromErr(err)
+	}
+
+	rolesToSet := []interface{}{}
+	for _, role := range roles.Roles {
+		rolesToSet = append(rolesToSet, role.ID)
+	}
+
+	result := multierror.Append(
+		d.Set("roles", rolesToSet), //TODO: actually populate!
+	)
+
+	return diag.FromErr(result.ErrorOrNil())
+}
+
+func updateOrganizationMember(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	if err := assignRoles(d, m); err != nil {
+		return diag.FromErr(fmt.Errorf("failed to assign members to organization. %w", err))
+	}
+
+	return readOrganizationMember(ctx, d, m)
+}
+
+func deleteOrganizationMember(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	api := m.(*management.Management)
+
+	orgID := String(d, "organization_id")
+	memberID := String(d, "user_id")
+
+	if err := api.Organization.DeleteMember(*orgID, []string{*memberID}); err != nil {
+		return diag.FromErr(err)
+	}
+
+	return nil
+}
---
+++ auth0/resource_auth0_organization_member_test.go
@@ -0,0 +1,173 @@
+package auth0
+
+import (
+	"fmt"
+	"log"
+	"math/rand"
+	"strings"
+	"testing"
+
+	"github.com/auth0/go-auth0/management"
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
+
+	"github.com/auth0/terraform-provider-auth0/auth0/internal/template"
+)
+
+func init() {
+	resource.AddTestSweepers("auth0_organization_member", &resource.Sweeper{
+		Name: "auth0_organization_member",
+		F: func(_ string) error {
+			api, err := Auth0()
+			if err != nil {
+				return err
+			}
+
+			var page int
+			var result *multierror.Error
+			for {
+				organizationList, err := api.Organization.List(management.Page(page))
+				if err != nil {
+					return err
+				}
+
+				for _, organization := range organizationList.Organizations {
+					log.Printf("[DEBUG] ➝ %s", organization.GetName())
+
+					if strings.Contains(organization.GetName(), "test") {
+						result = multierror.Append(
+							result,
+							api.Organization.Delete(organization.GetID()),
+						)
+
+						log.Printf("[DEBUG] ✗ %s", organization.GetName())
+					}
+				}
+				if !organizationList.HasNext() {
+					break
+				}
+				page++
+			}
+
+			users, err := api.User.List()
+			if err != nil {
+				return err
+			}
+			for _, user := range users.Users {
+				if strings.Contains(*user.Email, "test") {
+					err = api.User.Delete(*user.ID)
+					result = multierror.Append(result, err)
+				}
+			}
+
+			roles, err := api.Role.List()
+			if err != nil {
+				return err
+			}
+			for _, role := range roles.Roles {
+				if strings.Contains(*role.Name, "test") {
+					err = api.Role.Delete(*role.ID)
+					result = multierror.Append(result, err)
+				}
+			}
+
+			return result.ErrorOrNil()
+		},
+	})
+}
+
+func TestAccOrganizationMember(t *testing.T) {
+	httpRecorder := configureHTTPRecorder(t)
+
+	testName := strings.ToLower(t.Name() + fmt.Sprintf("%d", rand.Intn(931013557))) // TODO: REMOVE! This is only here to get around annoying 409s
+
+	resource.Test(t, resource.TestCase{
+		ProviderFactories: testProviders(httpRecorder),
+		Steps: []resource.TestStep{{
+			Config: template.ParseTestName(testAccOrganizationMembersAux+
+				`
+			resource auth0_organization_member member1 {
+				depends_on = [ auth0_user.user, auth0_organization.some_org ]
+				organization_id = auth0_organization.some_org.id
+				user_id = auth0_user.user.id
+			}`, testName),
+			Check: resource.ComposeTestCheckFunc(
+				resource.TestCheckResourceAttr("auth0_organization_member.member1", "roles.#", "0"),
+			),
+		},
+			{
+				Config: template.ParseTestName(testAccOrganizationMembersAux+
+					`
+			resource auth0_organization_member member1 {
+				depends_on = [ auth0_user.user, auth0_organization.some_org, auth0_role.reader ]
+				organization_id = auth0_organization.some_org.id
+				user_id = auth0_user.user.id
+				roles = [ auth0_role.reader.id ]
+			}`, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_organization_member.member1", "roles.#", "1"),
+				),
+			},
+			{
+				Config: template.ParseTestName(testAccOrganizationMembersAux+
+					`
+			resource auth0_organization_member member1 {
+				depends_on = [ auth0_user.user, auth0_organization.some_org, auth0_role.reader, auth0_role.admin ]
+				organization_id = auth0_organization.some_org.id
+				user_id = auth0_user.user.id
+				roles = [ auth0_role.reader.id, auth0_role.admin.id ]
+			}`, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_organization_member.member1", "roles.#", "2"),
+				),
+			},
+			{
+				Config: template.ParseTestName(testAccOrganizationMembersAux+
+					`
+			resource auth0_organization_member member1 {
+				depends_on = [ auth0_user.user, auth0_organization.some_org, auth0_role.reader, auth0_role.admin ]
+				organization_id = auth0_organization.some_org.id
+				user_id = auth0_user.user.id
+				roles = [ auth0_role.admin.id ]
+			}`, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_organization_member.member1", "roles.#", "1"),
+				),
+			},
+			{
+				Config: template.ParseTestName(testAccOrganizationMembersAux+
+					`
+			resource auth0_organization_member member1 {
+				depends_on = [ auth0_user.user, auth0_organization.some_org, auth0_role.reader, auth0_role.admin ]
+				organization_id = auth0_organization.some_org.id
+				user_id = auth0_user.user.id
+			}`, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_organization_member.member1", "roles.#", "0"),
+				),
+			},
+		},
+	})
+}
+
+const testAccOrganizationMembersAux = `
+resource auth0_role reader {
+	name = "Reader - {{.testName}}"
+}
+
+resource auth0_role admin {
+	name = "Admin - {{.testName}}"
+}
+
+resource auth0_user user {
+	email = "will.vedder+1{{.testName}}@auth0.com"
+	connection_name = "Username-Password-Authentication"
+	email_verified = true
+	password = "MyPass123$"
+}
+
+resource auth0_organization some_org{
+	name = "some-org-{{.testName}}"
+	display_name = "{{.testName}}"
+}
+`

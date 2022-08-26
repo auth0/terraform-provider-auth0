6b045fcd26b4767df285acdd950377fa519f502e
Sergiu Ghitea <sergiu.ghitea@okta.com>
2022-08-26 17:25:20 +0200

    Refactor auth0_prompt resource and add additional test case

--- internal/provider/resource_auth0_prompt_test.go
+++ internal/provider/resource_auth0_prompt_test.go
@@ -6,6 +6,30 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 )
 
+const testAccPromptCreate = `
+resource "auth0_prompt" "prompt" {
+  universal_login_experience = "classic"
+  identifier_first = false
+  webauthn_platform_first_factor = false
+}
+`
+
+const testAccPromptUpdate = `
+resource "auth0_prompt" "prompt" {
+  universal_login_experience = "new"
+  identifier_first = true
+  webauthn_platform_first_factor = false
+}
+`
+
+const testAccPromptUpdateAgain = `
+resource "auth0_prompt" "prompt" {
+  universal_login_experience = "new"
+  identifier_first = false
+  webauthn_platform_first_factor = true
+}
+`
+
 func TestAccPrompt(t *testing.T) {
 	httpRecorder := configureHTTPRecorder(t)
 
@@ -25,25 +49,17 @@ func TestAccPrompt(t *testing.T) {
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("auth0_prompt.prompt", "universal_login_experience", "new"),
 					resource.TestCheckResourceAttr("auth0_prompt.prompt", "identifier_first", "true"),
+					resource.TestCheckResourceAttr("auth0_prompt.prompt", "webauthn_platform_first_factor", "false"),
+				),
+			},
+			{
+				Config: testAccPromptUpdateAgain,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_prompt.prompt", "universal_login_experience", "new"),
+					resource.TestCheckResourceAttr("auth0_prompt.prompt", "identifier_first", "false"),
 					resource.TestCheckResourceAttr("auth0_prompt.prompt", "webauthn_platform_first_factor", "true"),
 				),
 			},
 		},
 	})
 }
-
-const testAccPromptCreate = `
-resource "auth0_prompt" "prompt" {
-  universal_login_experience = "classic"
-  identifier_first = false
-  webauthn_platform_first_factor = false
-}
-`
-
-const testAccPromptUpdate = `
-resource "auth0_prompt" "prompt" {
-  universal_login_experience = "new"
-  identifier_first = true
-  webauthn_platform_first_factor = true
-}
-`

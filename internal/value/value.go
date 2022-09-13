2f45a838dba9abd77fda1b4294bcacd58ea0431e
Sergiu Ghitea <sergiu.ghitea@okta.com>
2022-09-13 17:03:59 +0200

    Add more value funcs

--- internal/value/value.go
+++ internal/value/value.go
@@ -1,57 +1,90 @@
 package value
 
 import (
+	"log"
+
 	"github.com/hashicorp/go-cty/cty"
+	"github.com/hashicorp/go-cty/cty/gocty"
 )
 
-// Bool evaluates the typed value of the value and coerces to a pointer of a boolean.
+// Bool evaluates the typed value of the value
+// and coerces to a pointer of a boolean.
 func Bool(rawValue cty.Value) *bool {
 	if rawValue.IsNull() {
 		return nil
 	}
 
-	r := rawValue.True()
-	return &r
+	value := rawValue.True()
+	return &value
 }
 
-// String evaluates the typed value of the value and coerces to a pointer of a string.
+// String evaluates the typed value of the value
+// and coerces to a pointer of a string.
 func String(rawValue cty.Value) *string {
 	if rawValue.IsNull() {
 		return nil
 	}
 
-	r := rawValue.AsString()
-	return &r
+	value := rawValue.AsString()
+	return &value
 }
 
-// Int evaluates the typed value of the value and coerces to a pointer of an int.
+// Int evaluates the typed value of the value
+// and coerces to a pointer of an int.
 func Int(rawValue cty.Value) *int {
 	if rawValue.IsNull() {
 		return nil
 	}
 
-	intValue, _ := rawValue.AsBigFloat().Int64()
-
-	i := int(intValue)
-
-	return &i
+	int64Value, _ := rawValue.AsBigFloat().Int64()
+	value := int(int64Value)
+	return &value
 }
 
-// Strings evaluates the typed value of the value and coerces to a pointer of a slice of strings.
-func Strings(rawValue cty.Value) *[]string {
+// Float64 evaluates the typed value of the value
+// and coerces to a pointer of a float64.
+func Float64(rawValue cty.Value) *float64 {
 	if rawValue.IsNull() {
 		return nil
 	}
 
-	var s []string
-	for _, val := range rawValue.AsValueSlice() {
-		s = append(s, val.AsString())
-	}
-
-	return &s
+	value, _ := rawValue.AsBigFloat().Float64()
+	return &value
 }
 
-// MapOfStrings evaluates the typed value of the value and coerces to a pointer of a map of strings.
+// Strings evaluates the typed value of the value
+// and coerces to a pointer of a slice of strings.
+func Strings(rawValues cty.Value) *[]string {
+	if rawValues.IsNull() {
+		return nil
+	}
+
+	var value []string
+	for _, rawValue := range rawValues.AsValueSlice() {
+		value = append(value, rawValue.AsString())
+	}
+
+	return &value
+}
+
+// Map evaluates the typed value of the value
+// and coerces to a map[string]interface{}.
+func Map(rawValue cty.Value) map[string]interface{} {
+	if rawValue.IsNull() {
+		return nil
+	}
+
+	var value map[string]interface{}
+	if err := gocty.FromCtyValue(rawValue, &value); err != nil {
+		log.Printf("[WARN]: value.Map(): Failed to convert rawValue to map[string]interface{}: %v", err)
+		return nil
+	}
+
+	return value
+}
+
+// MapOfStrings evaluates the typed value of the value
+// and coerces to a pointer of a map of strings.
 func MapOfStrings(rawValue cty.Value) *map[string]string {
 	if rawValue.IsNull() {
 		return nil
--- internal/value/value_test.go
+++ internal/value/value_test.go
@@ -7,6 +7,7 @@ import (
 	"testing"
 
 	"github.com/hashicorp/go-cty/cty"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestBool(t *testing.T) {
@@ -65,6 +66,24 @@ func TestInt(t *testing.T) {
 	}
 }
 
+func TestFloat64(t *testing.T) {
+	v := Float64(cty.NullVal(cty.Number))
+	assert.Nil(t, v)
+
+	v = Float64(cty.NumberFloatVal(0))
+	assert.Equal(t, float64(0), *v)
+
+	v = Float64(cty.NumberFloatVal(-math.MaxFloat64))
+	if *v != -math.MaxFloat64 {
+		t.Errorf("Expected to be %v, got %v", -math.MaxFloat64, *v)
+	}
+
+	v = Float64(cty.NumberFloatVal(math.MaxFloat64))
+	if *v != math.MaxFloat64 {
+		t.Errorf("Expected to be %v, got %v", math.MaxFloat64, *v)
+	}
+}
+
 func TestStrings(t *testing.T) {
 	mockSliceVals := []string{"localhost/logout", "https://app.domain.com/logout"}
 	var mockSlice []cty.Value
@@ -85,6 +104,38 @@ func TestStrings(t *testing.T) {
 	if r != nil {
 		t.Errorf("expected to be nil, got %s", *r)
 	}
+}
+
+func TestMap(t *testing.T) {
+	v := Map(cty.MapValEmpty(cty.String))
+	if len(v) != 0 {
+		t.Errorf("expected to be empty map, got %v", v)
+	}
+
+	v = Map(cty.NilVal)
+	if v != nil {
+		t.Errorf("expected to be nil, got %v", v)
+	}
+
+	mockMap := cty.ObjectVal(map[string]cty.Value{
+		"logout": cty.StringVal("http://app.domain.com/logout"),
+		"login":  cty.StringVal("http://app.domain.com/login"),
+		"metadata": cty.ListVal([]cty.Value{
+			cty.StringVal("one"),
+			cty.StringVal("two"),
+		}),
+	})
+
+	v = Map(mockMap)
+	assert.Equal(
+		t,
+		map[string]interface{}{
+			"logout":   "http://app.domain.com/logout",
+			"login":    "http://app.domain.com/login",
+			"metadata": []string{"one", "two"},
+		},
+		v,
+	)
 }
 
 func TestMapOfStrings(t *testing.T) {

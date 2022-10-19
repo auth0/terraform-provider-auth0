9720b84e0c45007b1864d7b4b697154547cb6eb8
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2022-10-19 07:11:11 +0200

    [2/3] DXCDT-234: Fix update behavior of client_metadata (#362)
    * Fix update behavior or client_metadata
    
    * [1/3] DXCDT-243: Improve precision of branding theme fields with float64 instead of int (#369)
    
    Improve precision of branding theme fields with float64 instead of int

--- internal/provider/structure_auth0_client.go
+++ internal/provider/structure_auth0_client.go
@@ -40,7 +40,7 @@ func expandClient(d *schema.ResourceData) *management.Client {
 		TokenEndpointAuthMethod:        value.String(config.GetAttr("token_endpoint_auth_method")),
 		InitiateLoginURI:               value.String(config.GetAttr("initiate_login_uri")),
 		EncryptionKey:                  value.MapOfStrings(config.GetAttr("encryption_key")),
-		ClientMetadata:                 value.MapOfStrings(config.GetAttr("client_metadata")),
+		ClientMetadata:                 expandClientMetadata(d),
 		RefreshToken:                   expandClientRefreshToken(d),
 		JWTConfiguration:               expandClientJWTConfiguration(d),
 		Addons:                         expandClientAddons(d),
@@ -203,6 +203,24 @@ func expandClientMobileIOS(iosConfig cty.Value) *management.ClientMobileIOS {
 	}
 
 	return &ios
+}
+
+func expandClientMetadata(d *schema.ResourceData) *map[string]interface{} {
+	if !d.HasChange("client_metadata") {
+		return nil
+	}
+
+	oldMetadata, newMetadata := d.GetChange("client_metadata")
+	oldMetadataMap := oldMetadata.(map[string]interface{})
+	newMetadataMap := newMetadata.(map[string]interface{})
+
+	for key := range oldMetadataMap {
+		if _, ok := newMetadataMap[key]; !ok {
+			newMetadataMap[key] = nil
+		}
+	}
+
+	return &newMetadataMap
 }
 
 func expandClientAddons(d *schema.ResourceData) map[string]interface{} {

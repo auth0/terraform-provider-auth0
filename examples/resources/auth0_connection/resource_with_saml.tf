cb6851d018690610861eb7e078c4433767eb10b3
Will Vedder <will.vedder@okta.com>
2022-10-26 16:19:32 -0400

    SAML

--- examples/resources/auth0_connection/resource_with_saml.tf
+++ examples/resources/auth0_connection/resource_with_saml.tf
@@ -5,6 +5,7 @@ resource "auth0_connection" "samlp" {
   strategy = "samlp"
 
   options {
+    debug = false
     signing_cert        = "<signing-certificate>"
     sign_in_endpoint    = "https://saml.provider/sign_in"
     sign_out_endpoint   = "https://saml.provider/sign_out"
@@ -16,16 +17,31 @@ resource "auth0_connection" "samlp" {
     user_id_attribute   = "https://saml.provider/imi/ns/identity-200810"
     signature_algorithm = "rsa-sha256"
     digest_algorithm    = "sha256"
+    logo_url = "https://saml.provider/assets/logo.png"
+    entity_id = "<entity_id>"
+    metadata_xml = <<EOF
+    <?xml version="1.0"?>
+    <md:EntityDescriptor xmlns:md="urn:oasis:names:tc:SAML:2.0:metadata" xmlns:ds="http://www.w3.org/2000/09/xmldsig#" entityID="https://example.com">
+      <md:IDPSSODescriptor protocolSupportEnumeration="urn:oasis:names:tc:SAML:2.0:protocol">
+        <md:SingleLogoutService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://saml.provider/sign_out"/>
+        <md:SingleSignOnService Binding="urn:oasis:names:tc:SAML:2.0:bindings:HTTP-Redirect" Location="https://saml.provider/sign_in"/>
+      </md:IDPSSODescriptor>
+    </md:EntityDescriptor>
+    EOF 
     metadata_url        = "https://saml.provider/imi/ns/FederationMetadata.xml"
     fields_map = jsonencode({
       "name" : ["name", "nameidentifier"]
       "email" : ["emailaddress", "nameidentifier"]
       "family_name" : "surname"
     })
-
     signing_key {
       key  = "-----BEGIN PRIVATE KEY-----\n...{your private key here}...\n-----END PRIVATE KEY-----"
       cert = "-----BEGIN CERTIFICATE-----\n...{your public key cert here}...\n-----END CERTIFICATE-----"
     }
+    idp_initiated {
+			client_id = "client_id"
+			client_protocol = "samlp"
+			client_authorize_query = "type=code&timeout=30"
+		}
   }
 }

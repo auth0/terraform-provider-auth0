3c8773c3c6835a39fc50ed9b38c157981e8fc6ae
Will Vedder <will.vedder@okta.com>
2022-10-26 15:55:31 -0400

    Google apps

--- examples/resources/auth0_connection/resource_with_google_apps.tf
+++ examples/resources/auth0_connection/resource_with_google_apps.tf
@@ -11,10 +11,13 @@ resource "auth0_connection" "google_apps" {
     domain_aliases   = ["example.com", "api.example.com"]
     api_enable_users = true
     scopes           = ["ext_profile", "ext_groups"]
+    icon_url = "http://example.com/assets/logo.png"
     upstream_params = jsonencode({
       "screen_name" : {
         "alias" : "login_hint"
       }
     })
+    set_user_root_attributes = "on_each_login"
+    non_persistent_attrs = ["ethnicity","gender"]
   }
 }

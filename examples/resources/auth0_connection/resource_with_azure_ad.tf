ffc3cda7425c716ea5279bc599bd5131db5ecf72
Will Vedder <will.vedder@okta.com>
2022-10-26 15:14:53 -0400

    WAAD

--- examples/resources/auth0_connection/resource_with_azure_ad.tf
+++ examples/resources/auth0_connection/resource_with_azure_ad.tf
@@ -6,15 +6,18 @@ resource "auth0_connection" "azure_ad" {
     identity_api  = "azure-active-directory-v1.0"
     client_id     = "123456"
     client_secret = "123456"
+    app_id = "app-id-123"
     tenant_domain = "example.onmicrosoft.com"
     domain        = "example.onmicrosoft.com"
     domain_aliases = [
       "example.com",
       "api.example.com"
     ]
+    icon_url = "http://example.onmicrosoft.com/assets/logo.png"
     use_wsfed            = false
     waad_protocol        = "openid-connect"
     waad_common_endpoint = false
+    max_groups_to_retrieve = 250
     api_enable_users     = true
     scopes = [
       "basic_profile",
@@ -28,5 +31,6 @@ resource "auth0_connection" "azure_ad" {
         "alias" : "login_hint"
       }
     })
+    non_persistent_attrs = ["ethnicity", "gender"]
   }
 }

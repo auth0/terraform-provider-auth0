686a54853c371e99643cc5477f8adc8cb54d3372
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2023-02-10 19:05:36 +0100

    Expand tenant data source

--- internal/auth0/tenant/data_source.go
+++ internal/auth0/tenant/data_source.go
@@ -10,42 +10,53 @@ import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/resource"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	internalSchema "github.com/auth0/terraform-provider-auth0/internal/schema"
 )
 
 // NewDataSource will return a new auth0_tenant data source.
 func NewDataSource() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: readDataTenant,
+		ReadContext: readTenantForDataSource,
 		Description: "Use this data source to access information about the tenant this provider is configured to access.",
-		Schema: map[string]*schema.Schema{
-			"domain": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "Your Auth0 domain name.",
-			},
-			"management_api_identifier": {
-				Type:     schema.TypeString,
-				Computed: true,
-				Description: "The identifier value of the built-in Management API resource server, " +
-					"which can be used as an audience when configuring client grants.",
-			},
-		},
+		Schema:      dataSourceSchema(),
 	}
 }
 
-func readDataTenant(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
-	api := m.(*management.Management)
+func dataSourceSchema() map[string]*schema.Schema {
+	dataSourceSchema := internalSchema.TransformResourceToDataSource(NewResource().Schema)
+	dataSourceSchema["domain"] = &schema.Schema{
+		Type:        schema.TypeString,
+		Computed:    true,
+		Description: "Your Auth0 domain name.",
+	}
+	dataSourceSchema["management_api_identifier"] = &schema.Schema{
+		Type:     schema.TypeString,
+		Computed: true,
+		Description: "The identifier value of the built-in Management API resource server, " +
+			"which can be used as an audience when configuring client grants.",
+	}
+
+	return dataSourceSchema
+}
+
+func readTenantForDataSource(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api := meta.(*management.Management)
 
 	u, err := url.Parse(api.URI())
 	if err != nil {
 		return diag.FromErr(fmt.Errorf("unable to determine management API URL: %w", err))
 	}
 
-	d.SetId(resource.UniqueId())
+	data.SetId(resource.UniqueId())
 
-	var result *multierror.Error
-	result = multierror.Append(result, d.Set("domain", u.Hostname()))
-	result = multierror.Append(result, d.Set("management_api_identifier", u.String()))
+	result := multierror.Append(
+		data.Set("domain", u.Hostname()),
+		data.Set("management_api_identifier", u.String()),
+	)
+	if result.ErrorOrNil() != nil {
+		return diag.FromErr(result.ErrorOrNil())
+	}
 
-	return diag.FromErr(result.ErrorOrNil())
+	return readTenant(ctx, data, meta)
 }

7d354a3f87a409b8b045c387abec1ecd13a986be
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2023-02-22 10:58:49 +0100

    Simplify management of branding themes

--- internal/auth0/branding/data_source_theme.go
+++ internal/auth0/branding/data_source_theme.go
@@ -1,9 +1,6 @@
 package branding
 
 import (
-	"context"
-
-	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 
 	internalSchema "github.com/auth0/terraform-provider-auth0/internal/schema"
@@ -12,26 +9,12 @@ import (
 // NewThemeDataSource will return a new auth0_branding_theme data source.
 func NewThemeDataSource() *schema.Resource {
 	return &schema.Resource{
-		ReadContext: readBrandingThemeForDataSource,
+		ReadContext: readBrandingTheme,
 		Description: "Use this data source to access information about the tenant's branding theme settings.",
 		Schema:      themeDataSourceSchema(),
 	}
 }
 
 func themeDataSourceSchema() map[string]*schema.Schema {
-	dataSourceSchema := internalSchema.TransformResourceToDataSource(NewThemeResource().Schema)
-
-	dataSourceSchema["branding_theme_id"] = &schema.Schema{
-		Type:        schema.TypeString,
-		Required:    true,
-		Description: "The ID of the branding theme.",
-	}
-
-	return dataSourceSchema
-}
-
-func readBrandingThemeForDataSource(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
-	brandingThemeID := data.Get("branding_theme_id").(string)
-	data.SetId(brandingThemeID)
-	return readBrandingTheme(ctx, data, meta)
+	return internalSchema.TransformResourceToDataSource(NewThemeResource().Schema)
 }

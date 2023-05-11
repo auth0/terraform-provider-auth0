577845c13e170928a5402b73ac62177b11a3cab1
Will Vedder <willvedd@gmail.com>
2023-05-11 10:33:29 -0400

    DXCDT-427: User-permission resource (2/2) (#574)
    * Initial commit for user permission relationship resource
    
    * Adding read-only permissions field to user resource and data source
    
    * Typeset and rolling-back doc change
    
    * Setting ID properly and generating docs
    
    * Regenerating docs, regenerating test recordings
    
    * Regenerating another test
    
    * Regenerating another test
    
    * Adding read-only fields and import function
    
    * Update internal/auth0/user/resource.go
    
    Co-authored-by: Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
    
    * Update internal/auth0/user/resource.go
    
    Co-authored-by: Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
    
    * Update internal/auth0/user/resource.go
    
    Co-authored-by: Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
    
    * Stronger test
    
    * Stronger tests and re-recording
    
    * Updating docs
    
    * Updating docs, providing example, fixing import
    
    * Fixing ID set
    
    * Minding errors, fixing import delimiter, fixing docs
    
    * Simplifying tests, re-recording
    
    * Update internal/auth0/user/resource_permission.go
    
    Co-authored-by: Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
    
    * Converting mutex
    
    * Regenerating docs
    
    * Converting mutex
    
    * Apply suggestions from code review
    
    Co-authored-by: Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
    
    * Addressing PR feedback
    
    ---------
    
    Co-authored-by: Will Vedder <will.vedder@okta.com>
    Co-authored-by: Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>

---
+++ internal/auth0/user/resource_permission.go
@@ -0,0 +1,172 @@
+package user
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strings"
+
+	"github.com/auth0/go-auth0/management"
+	"github.com/hashicorp/go-multierror"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/auth0/terraform-provider-auth0/internal/config"
+)
+
+// NewPermissionResource will return a new auth0_connection_client resource.
+func NewPermissionResource() *schema.Resource {
+	return &schema.Resource{
+		Schema: map[string]*schema.Schema{
+			"user_id": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "ID of the user to associate the permission to.",
+			},
+			"permission": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Name of the permission.",
+			},
+			"resource_server_identifier": {
+				Type:        schema.TypeString,
+				Required:    true,
+				ForceNew:    true,
+				Description: "Identifier of the resource server that the permission is associated with.",
+			},
+			"description": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Description of the permission.",
+			},
+			"resource_server_name": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "Name of the resource server that the permission is associated with.",
+			},
+		},
+		CreateContext: createUserPermission,
+		ReadContext:   readUserPermission,
+		DeleteContext: deleteUserPermission,
+		Importer: &schema.ResourceImporter{
+			StateContext: importUserPermission,
+		},
+		Description: "With this resource, you can manage user permissions.",
+	}
+}
+
+func createUserPermission(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api := meta.(*config.Config).GetAPI()
+	mutex := meta.(*config.Config).GetMutex()
+
+	userID := data.Get("user_id").(string)
+	resourceServerID := data.Get("resource_server_identifier").(string)
+	permissionName := data.Get("permission").(string)
+
+	mutex.Lock(userID)
+	defer mutex.Unlock(userID)
+
+	if err := api.User.AssignPermissions(userID, []*management.Permission{
+		{
+			ResourceServerIdentifier: &resourceServerID,
+			Name:                     &permissionName,
+		},
+	}); err != nil {
+		return diag.FromErr(err)
+	}
+
+	data.SetId(fmt.Sprintf(`%s::%s::%s`, userID, resourceServerID, permissionName))
+
+	return readUserPermission(ctx, data, meta)
+}
+
+func readUserPermission(_ context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api := meta.(*config.Config).GetAPI()
+
+	userID := data.Get("user_id").(string)
+	permissionName := data.Get("permission").(string)
+	resourceServerID := data.Get("resource_server_identifier").(string)
+
+	existingPermissions, err := api.User.Permissions(userID)
+	if err != nil {
+		if mErr, ok := err.(management.Error); ok && mErr.Status() == http.StatusNotFound {
+			data.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	for _, p := range existingPermissions.Permissions {
+		if p.GetName() == permissionName && p.GetResourceServerIdentifier() == resourceServerID {
+			result := multierror.Append(
+				data.Set("description", p.GetDescription()),
+				data.Set("resource_server_name", p.GetResourceServerName()),
+			)
+			return diag.FromErr(result.ErrorOrNil())
+		}
+	}
+
+	data.SetId("")
+	return nil
+}
+
+func deleteUserPermission(_ context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api := meta.(*config.Config).GetAPI()
+	mutex := meta.(*config.Config).GetMutex()
+
+	userID := data.Get("user_id").(string)
+	permissionName := data.Get("permission").(string)
+	resourceServerID := data.Get("resource_server_identifier").(string)
+
+	mutex.Lock(userID)
+	defer mutex.Unlock(userID)
+
+	if err := api.User.RemovePermissions(
+		userID,
+		[]*management.Permission{
+			{
+				ResourceServerIdentifier: &resourceServerID,
+				Name:                     &permissionName,
+			},
+		},
+	); err != nil {
+		if mErr, ok := err.(management.Error); ok && mErr.Status() == http.StatusNotFound {
+			data.SetId("")
+			return nil
+		}
+		return diag.FromErr(err)
+	}
+
+	data.SetId("")
+	return nil
+}
+
+func importUserPermission(
+	_ context.Context,
+	data *schema.ResourceData,
+	_ interface{},
+) ([]*schema.ResourceData, error) {
+	rawID := data.Id()
+	if rawID == "" {
+		return nil, fmt.Errorf("ID cannot be empty")
+	}
+
+	if !strings.Contains(rawID, "::") {
+		return nil, fmt.Errorf("ID must be formatted as <userID>::<resourceServerIdentifier>::<permission>")
+	}
+
+	idPair := strings.Split(rawID, "::")
+	if len(idPair) != 3 {
+		return nil, fmt.Errorf("ID must be formatted as <userID>::<resourceServerIdentifier>::<permission>")
+	}
+
+	result := multierror.Append(
+		data.Set("user_id", idPair[0]),
+		data.Set("resource_server_identifier", idPair[1]),
+		data.Set("permission", idPair[2]),
+	)
+
+	return []*schema.ResourceData{data}, result.ErrorOrNil()
+}
---
+++ internal/auth0/user/resource_permission_test.go
@@ -0,0 +1,126 @@
+package user_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/auth0/terraform-provider-auth0/internal/acctest"
+)
+
+const givenAResourceServerAndUser = `
+resource "auth0_resource_server" "resource_server" {
+	name = "Acceptance Test - {{.testName}}"
+	identifier = "https://uat.api.terraform-provider-auth0.com/{{.testName}}"
+	scopes {
+		value = "read:foo"
+		description = "Can read Foo"
+	}
+	scopes {
+		value = "create:foo"
+		description = "Can create Foo"
+	}
+}
+
+resource "auth0_user" "user" { 
+	depends_on = [ auth0_resource_server.resource_server ] 
+	connection_name = "Username-Password-Authentication" 
+	user_id = "{{.testName}}" 
+	username = "{{.testName}}" 
+	password = "passpass$12$12" 
+	email = "{{.testName}}@acceptance.test.com" 
+}
+`
+
+const givenAUserPermission = `
+resource "auth0_user_permission" "user_permission_read" {
+	depends_on = [ auth0_resource_server.resource_server, auth0_user.user ]
+
+	user_id = auth0_user.user.id
+	resource_server_identifier = auth0_resource_server.resource_server.identifier
+	permission = "read:foo"
+}
+`
+
+const givenAnotherUserPermission = `
+resource "auth0_user_permission" "user_permission_create" {
+	depends_on = [ auth0_resource_server.resource_server, auth0_user.user ]
+
+	user_id = auth0_user.user.id
+	resource_server_identifier = auth0_resource_server.resource_server.identifier
+	permission = "create:foo"
+}
+`
+
+const testAccUserPermissionNoneAssigned = givenAResourceServerAndUser
+const testAccUserPermissionOneAssigned = givenAResourceServerAndUser + givenAUserPermission
+const testAccUserPermissionTwoAssigned = givenAResourceServerAndUser + givenAUserPermission + givenAnotherUserPermission
+
+func TestAccUserPermission(t *testing.T) {
+	acctest.Test(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: acctest.ParseTestName(testAccUserPermissionNoneAssigned, strings.ToLower(t.Name())),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.#", "0"),
+				),
+			},
+			{
+				Config: acctest.ParseTestName(testAccUserPermissionOneAssigned, strings.ToLower(t.Name())),
+			},
+			{
+				RefreshState: true,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.#", "1"),
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.0.name", "read:foo"),
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.0.resource_server_identifier", "https://uat.api.terraform-provider-auth0.com/testaccuserpermission"),
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.0.resource_server_name", "Acceptance Test - testaccuserpermission"),
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.0.description", "Can read Foo"),
+
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_read", "permission", "read:foo"),
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_read", "user_id", "auth0|testaccuserpermission"),
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_read", "resource_server_identifier", "https://uat.api.terraform-provider-auth0.com/testaccuserpermission"),
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_read", "resource_server_name", "Acceptance Test - testaccuserpermission"),
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_read", "description", "Can read Foo"),
+				),
+			},
+			{
+				Config: acctest.ParseTestName(testAccUserPermissionTwoAssigned, strings.ToLower(t.Name())),
+			},
+			{
+				RefreshState: true,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.#", "2"),
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.0.name", "create:foo"),
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.1.name", "read:foo"),
+
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_create", "permission", "create:foo"),
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_create", "user_id", "auth0|testaccuserpermission"),
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_create", "resource_server_identifier", "https://uat.api.terraform-provider-auth0.com/testaccuserpermission"),
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_create", "resource_server_name", "Acceptance Test - testaccuserpermission"),
+					resource.TestCheckResourceAttr("auth0_user_permission.user_permission_create", "description", "Can create Foo"),
+				),
+			},
+			{
+				Config: acctest.ParseTestName(testAccUserPermissionOneAssigned, strings.ToLower(t.Name())),
+			},
+			{
+				RefreshState: true,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.#", "1"),
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.0.name", "read:foo"),
+				),
+			},
+			{
+				Config: acctest.ParseTestName(testAccUserPermissionNoneAssigned, strings.ToLower(t.Name())),
+			},
+			{
+				RefreshState: true,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_user.user", "permissions.#", "0"),
+				),
+			},
+		},
+	})
+}

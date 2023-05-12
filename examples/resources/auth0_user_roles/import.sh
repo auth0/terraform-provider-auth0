8ca33e0137958786e3424c79ae289fae42597b71
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2023-05-12 18:13:05 +0200

    DXCDT-434: Add new auth0_user_roles resource (#579)

---
+++ examples/resources/auth0_user_roles/import.sh
@@ -0,0 +1,4 @@
+# This resource can be imported using the user ID.
+#
+# Example:
+terraform import auth0_user_roles.user_roles "auth0|111111111111111111111111"
---
+++ internal/auth0/user/resource_roles_test.go
@@ -0,0 +1,155 @@
+package user_test
+
+import (
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+
+	"github.com/auth0/terraform-provider-auth0/internal/acctest"
+)
+
+const testAccUserRolesUpdateUserWithOneRoleAssigned = `
+resource auth0_role owner {
+	name = "owner"
+	description = "Owner"
+}
+
+resource auth0_user user {
+	depends_on = [auth0_role.owner]
+
+	connection_name = "Username-Password-Authentication"
+	email = "{{.testName}}@acceptance.test.com"
+	password = "passpass$12$12"
+
+	lifecycle {
+		ignore_changes = [roles]
+	}
+}
+
+resource auth0_user_roles user_roles {
+	depends_on = [ auth0_user.user ]
+
+	user_id = auth0_user.user.id
+	roles = [auth0_role.owner.id]
+}
+
+data auth0_user user_data {
+	depends_on = [auth0_user_roles.user_roles]
+
+	user_id = auth0_user.user.id
+}
+`
+
+const testAccUserRolesUpdateUserWithTwoRolesAssigned = `
+resource auth0_role owner {
+	name = "owner"
+	description = "Owner"
+}
+
+resource auth0_role admin {
+	name = "admin"
+	description = "Administrator"
+}
+
+resource auth0_user user {
+	depends_on = [auth0_role.owner, auth0_role.admin]
+
+	connection_name = "Username-Password-Authentication"
+	email = "{{.testName}}@acceptance.test.com"
+	password = "passpass$12$12"
+
+	lifecycle {
+		ignore_changes = [roles]
+	}
+}
+
+resource auth0_user_roles user_roles {
+	depends_on = [ auth0_user.user ]
+
+	user_id = auth0_user.user.id
+	roles = [auth0_role.owner.id, auth0_role.admin.id]
+}
+
+data auth0_user user_data {
+	depends_on = [auth0_user_roles.user_roles]
+
+	user_id = auth0_user.user.id
+}
+`
+
+const testAccUserRolesUpdateUserWithNoRolesAssigned = `
+resource auth0_user user {
+	connection_name = "Username-Password-Authentication"
+	email = "{{.testName}}@acceptance.test.com"
+	password = "passpass$12$12"
+
+	lifecycle {
+		ignore_changes = [roles]
+	}
+}
+
+resource auth0_user_roles user_roles {
+	depends_on = [ auth0_user.user ]
+
+	user_id = auth0_user.user.id
+	roles = []
+}
+
+data auth0_user user_data {
+	depends_on = [auth0_user_roles.user_roles]
+
+	user_id = auth0_user.user.id
+}
+`
+
+const testAccUserRolesDeleteResource = `
+resource auth0_user user {
+	connection_name = "Username-Password-Authentication"
+	email = "{{.testName}}@acceptance.test.com"
+	password = "passpass$12$12"
+}
+`
+
+func TestAccUserRoles(t *testing.T) {
+	testName := strings.ToLower(t.Name())
+
+	acctest.Test(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: acctest.ParseTestName(testAccUserRolesUpdateUserWithOneRoleAssigned, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.auth0_user.user_data", "roles.#", "1"),
+					resource.TestCheckResourceAttr("auth0_user_roles.user_roles", "roles.#", "1"),
+				),
+			},
+			{
+				Config: acctest.ParseTestName(testAccUserRolesUpdateUserWithTwoRolesAssigned, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.auth0_user.user_data", "roles.#", "2"),
+					resource.TestCheckResourceAttr("auth0_user_roles.user_roles", "roles.#", "2"),
+				),
+			},
+			{
+				Config: acctest.ParseTestName(testAccUserRolesUpdateUserWithNoRolesAssigned, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.auth0_user.user_data", "roles.#", "0"),
+					resource.TestCheckResourceAttr("auth0_user_roles.user_roles", "roles.#", "0"),
+				),
+			},
+			{
+				Config: acctest.ParseTestName(testAccUserRolesUpdateUserWithOneRoleAssigned, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.auth0_user.user_data", "roles.#", "1"),
+					resource.TestCheckResourceAttr("auth0_user_roles.user_roles", "roles.#", "1"),
+				),
+			},
+			{
+				Config: acctest.ParseTestName(testAccUserRolesDeleteResource, testName),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_user.user", "roles.#", "0"),
+				),
+			},
+		},
+	})
+}

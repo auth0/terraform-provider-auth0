2e0778565db084931ecc4ebcdb281842af954f8e
Will Vedder <will.vedder@okta.com>
2023-05-31 13:23:28 -0400

    Tests

--- internal/auth0/action/resource_trigger_action_test.go
+++ internal/auth0/action/resource_trigger_action_test.go
@@ -1,6 +1,8 @@
 package action_test
 
 import (
+	"os"
+	"strings"
 	"testing"
 
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
@@ -36,7 +38,6 @@ const testAccCreateAnotherTriggerAction = `
 			id = "post-login"
 			version = "v3"
 		}
-
 		deploy = true
 	}
 
@@ -65,6 +66,61 @@ func TestAccTriggerAction(t *testing.T) {
 			},
 			{
 				Config: acctest.ParseTestName(testAccCreateTriggerAction, t.Name()),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_trigger_action.my_action_post_login", "trigger", "post-login"),
+					resource.TestCheckResourceAttrSet("auth0_trigger_action.my_action_post_login", "action_id"),
+				),
+			},
+		},
+	})
+}
+
+const testAccImportTriggerAction = `
+	resource auth0_action my_action {
+		name = "Test Action testaccimporttriggeraction"
+		code = "exports.onExecutePostLogin = async (event, api) => {};"
+
+		supported_triggers {
+			id = "post-login"
+			version = "v3"
+		}
+	}
+
+	resource auth0_trigger_action my_action_post_login {
+		action_id = auth0_action.my_action.id
+		trigger = tolist(auth0_action.my_action.supported_triggers)[0].id
+	}
+`
+
+func TestAccTriggerActionImport(t *testing.T) {
+	if os.Getenv("AUTH0_DOMAIN") != acctest.RecordingsDomain {
+		// The test runs only with recordings as it requires an initial setup.
+		t.Skip()
+	}
+
+	testName := strings.ToLower(t.Name())
+
+	importedActionID := "a6bc1597-1401-4635-b1df-de27e500ff3c"
+
+	acctest.Test(t, resource.TestCase{
+		PreventPostDestroyRefresh: true,
+		Steps: []resource.TestStep{
+			{
+				Config:             acctest.ParseTestName(testAccImportTriggerAction, testName),
+				ResourceName:       "auth0_action.my_action",
+				ImportState:        true,
+				ImportStateId:      importedActionID,
+				ImportStatePersist: true,
+			},
+			{
+				Config:             acctest.ParseTestName(testAccImportTriggerAction, testName),
+				ResourceName:       "auth0_trigger_action.my_action_post_login",
+				ImportState:        true,
+				ImportStateId:      "post-login::" + importedActionID,
+				ImportStatePersist: true,
+			},
+			{
+				Config: acctest.ParseTestName(testAccImportTriggerAction, testName),
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("auth0_trigger_action.my_action_post_login", "trigger", "post-login"),
 					resource.TestCheckResourceAttrSet("auth0_trigger_action.my_action_post_login", "action_id"),

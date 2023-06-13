b84a0d9a6afbf7c1706071075efeb98fbadacf58
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2023-06-13 10:25:13 +0200

    Fix import issue in auth0_user_role resource

--- examples/resources/auth0_user_permission/import.sh
+++ examples/resources/auth0_user_permission/import.sh
@@ -1,7 +1,6 @@
 # This resource can be imported by specifying the
 # user ID, resource identifier and permission name separated by "::" (note the double colon)
 # <userID>::<resourceServerIdentifier>::<permission>
-
 #
 # Example:
 terraform import auth0_user_permission.permission "auth0|111111111111111111111111::https://api.travel0.com/v1::read:posts"

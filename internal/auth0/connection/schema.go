d66c0d6d0e419cc4c4074a2752057abf31bba798
Mateusz Wroński <mateusz.wronski@smartbear.com>
2023-07-03 19:09:06 +0200

    Update internal/auth0/connection/schema.go
    review, pt1
    
    Co-authored-by: Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>

--- internal/auth0/connection/schema.go
+++ internal/auth0/connection/schema.go
@@ -570,7 +570,7 @@ var resourceSchemaV0 = map[string]*schema.Schema{
 					Type:        schema.TypeString,
 					Optional:    true,
 					Description: "Value can be `back_channel` or `front_channel`. " +
-						"Front Channel will use OIDC protocol with response_mode=form_post and response_type=id_token. " +
+						"Front Channel will use OIDC protocol with `response_mode=form_post` and `response_type=id_token`. " +
  						"Back Channel will use response_type=code.",
 				},
 				"issuer": {

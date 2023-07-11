49114e12e66b0f8c435a7aeef3c951ce3713724b
Will Vedder <will.vedder@okta.com>
2023-07-11 17:30:45 -0400

    Adding tests and recordings

---
+++ internal/auth0/page/data_source.go
@@ -0,0 +1,31 @@
+package page
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/id"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	internalSchema "github.com/auth0/terraform-provider-auth0/internal/schema"
+)
+
+// NewDataSource will return a new auth0_page data source.
+func NewDataSource() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: readPagesForDataSource,
+		Description: "Read custom HTML for the login, reset password, multi-factor authentication and error pages.",
+		Schema:      dataSourceSchema(),
+	}
+}
+
+func dataSourceSchema() map[string]*schema.Schema {
+	dataSourceSchema := internalSchema.TransformResourceToDataSource(NewResource().Schema)
+	return dataSourceSchema
+}
+
+func readPagesForDataSource(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	// This resource is not identified by an id in the Auth0 management API.
+	data.SetId(id.UniqueId())
+	return readPages(ctx, data, meta)
+}

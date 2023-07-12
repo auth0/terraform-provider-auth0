bef0850f535fd6ca3f06d31c2638f558e8099341
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2023-07-12 17:52:19 +0200

    Remove scope field from resource server resource

--- internal/auth0/role/data_source_test.go
+++ internal/auth0/role/data_source_test.go
@@ -15,28 +15,37 @@ const testAccGivenAResourceServer = `
 resource "auth0_resource_server" "matrix" {
     name       = "Role - Acceptance Test - {{.testName}}"
     identifier = "https://{{.testName}}.matrix.com/"
+}
 
-    scopes {
-        value = "stop:bullets"
-        description = "Stop bullets"
-    }
+resource "auth0_resource_server_scopes" "my_api_scopes" {
+	depends_on = [ auth0_resource_server.matrix ]
 
-    scopes {
-        value = "bring:peace"
-        description = "Bring peace"
-    }
+	resource_server_identifier = auth0_resource_server.matrix.identifier
+
+	scopes {
+		name        = "stop:bullets"
+		description = "Stop bullets"
+	}
+
+	scopes {
+		name        = "bring:peace"
+		description = "Bring peace"
+	}
 }
 
 resource "auth0_role" "the_one" {
+	depends_on = [ auth0_resource_server_scopes.my_api_scopes ]
+
 	name        = "The One - Acceptance Test - {{.testName}}"
 	description = "The One - Acceptance Test"
 
 	permissions {
-		name = "stop:bullets"
+		name                       = "stop:bullets"
 		resource_server_identifier = auth0_resource_server.matrix.identifier
 	}
+
 	permissions {
-		name = "bring:peace"
+		name                       = "bring:peace"
 		resource_server_identifier = auth0_resource_server.matrix.identifier
 	}
 }
@@ -102,7 +111,7 @@ func TestAccDataSourceRoleByID(t *testing.T) {
 	})
 }
 
-const testAccDataSourceNonexistentRole = testAccGivenAResourceServer + `
+const testAccDataSourceNonexistentRole = `
 data "auth0_role" "test" {
 	name = "this-role-does-not-exist"
 }

d0ed2b848c4b25a2833bd828bc0ff013b0b9c132
Kunal Dawar <kunal.dawar@okta.com>
2024-05-27 14:56:12 +0530

    (GH-925) Fix pagination for auth0_organization_member import/Create limit

--- internal/auth0/organization/resource_member.go
+++ internal/auth0/organization/resource_member.go
@@ -57,15 +57,15 @@ func readOrganizationMember(ctx context.Context, data *schema.ResourceData, meta
 	api := meta.(*config.Config).GetAPI()
 
 	organizationID := data.Get("organization_id").(string)
+	userID := data.Get("user_id").(string)
 
-	members, err := api.Organization.Members(ctx, organizationID)
+	members, err := fetchAllOrganizationMembers(ctx, api, organizationID)
 	if err != nil {
 		return diag.FromErr(internalError.HandleAPIError(data, err))
 	}
 
-	userID := data.Get("user_id").(string)
-	for _, member := range members.Members {
-		if member.GetUserID() == userID {
+	for _, memberID := range members {
+		if memberID == userID {
 			return nil
 		}
 	}

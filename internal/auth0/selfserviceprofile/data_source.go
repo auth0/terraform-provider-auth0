d8f756ff71d106e4680b1df3e5bb8af208c0bbb9
Rajat Bajaj <rajat.bajaj@okta.com>
2024-08-23 02:18:25 +0530

    Added schema, test cases and documentation for Self Service Profile SSO

---
+++ internal/auth0/selfserviceprofile/data_source.go
@@ -0,0 +1,50 @@
+package selfserviceprofile
+
+import (
+	"context"
+
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+
+	"github.com/auth0/terraform-provider-auth0/internal/config"
+	internalSchema "github.com/auth0/terraform-provider-auth0/internal/schema"
+)
+
+// NewDataSource will return a new auth0_self_service_profile data source.
+func NewDataSource() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: readSelfServiceProfileForDataSource,
+		Description: "Data source to retrieve a specific Auth0 Self-Service Profile by `id`",
+		Schema:      dataSourceSchema(),
+	}
+}
+
+func dataSourceSchema() map[string]*schema.Schema {
+	dataSourceSchema := internalSchema.TransformResourceToDataSource(NewResource().Schema)
+	dataSourceSchema["id"] = &schema.Schema{
+		Type:        schema.TypeString,
+		Required:    true,
+		Description: "The ID of the Self Service Profile.",
+	}
+	dataSourceSchema["id"].Description = "The id of the Self Service Profile "
+	return dataSourceSchema
+}
+
+func readSelfServiceProfileForDataSource(ctx context.Context, data *schema.ResourceData, meta interface{}) diag.Diagnostics {
+	api := meta.(*config.Config).GetAPI()
+
+	id := data.Get("id").(string)
+	if id != "" {
+		data.SetId(id)
+
+		ssp, err := api.SelfServiceProfile.Read(ctx, data.Id())
+		if err != nil {
+			return diag.FromErr(err)
+		}
+
+		err = flattenSelfServiceProfile(data, ssp)
+
+		return diag.FromErr(err)
+	}
+	return diag.Errorf("No Self Service Profile found with \"id\" = %q", id)
+}

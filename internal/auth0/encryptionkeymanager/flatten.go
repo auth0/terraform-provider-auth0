1a3b62860b488a3090aeb6f52cd8def536a5076e
A. Craig West <craig.west@okta.com>
2024-10-03 09:59:47 -0400

    Add auth0_encryption_keys resource to allow rekeying of encryption keys (#1031)
    * Add auth0_encryption_key resource to allow rekeying of tenant encryption keys
    
    * Slight renaming, and use key_rotation_id instead of rekey attribute
    
    * Rename auth0_encryption_key to auth0_encryption_key_manager

---
+++ internal/auth0/encryptionkeymanager/flatten.go
@@ -0,0 +1,22 @@
+package encryptionkeymanager
+
+import (
+	"github.com/auth0/go-auth0/management"
+)
+
+func flattenEncryptionKeys(keys []*management.EncryptionKey) []interface{} {
+	var result []interface{}
+	const timeRFC3339WithMilliseconds = "2006-01-02T15:04:05.000Z07:00"
+
+	for _, key := range keys {
+		result = append(result, map[string]interface{}{
+			"key_id":        key.GetKID(),
+			"parent_key_id": key.GetParentKID(),
+			"type":          key.GetType(),
+			"state":         key.GetState(),
+			"created_at":    key.GetCreatedAt().Format(timeRFC3339WithMilliseconds),
+			"updated_at":    key.GetUpdatedAt().Format(timeRFC3339WithMilliseconds),
+		})
+	}
+	return result
+}

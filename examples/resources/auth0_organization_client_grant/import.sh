0a519d6901785921364192ade384f87213164e6a
Rajat Bajaj <rajat.bajaj@okta.com>
2024-11-15 23:15:16 +0530

    Added example for org client grant

---
+++ examples/resources/auth0_organization_client_grant/import.sh
@@ -0,0 +1,6 @@
+# This resource can be imported by specifying the
+# organization ID and connection ID separated by "::" (note the double colon)
+# <organizationID>::<connectionID>
+#
+# Example:
+terraform import auth0_organization_connection.my_org_conn "org_XXXXX::con_XXXXX"

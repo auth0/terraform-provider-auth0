fce27dc7eb7c4de2d6de66478792c634845f1dba
Rajat Bajaj <rajat.bajaj@okta.com>
2024-11-15 22:58:25 +0530

    Added docs and examples

--- examples/resources/auth0_self_service_profile_custom_text/resource.tf
+++ examples/resources/auth0_self_service_profile_custom_text/resource.tf
@@ -1,7 +1,7 @@
 resource "auth0_self_service_profile_custom_text" "sso_custom_text" {
-    sso_id = "some-sso-id"
-    language = "en"
-    page = "get-started"
-    body = "{}"
+  sso_id   = "some-sso-id"
+  language = "en"
+  page     = "get-started"
+  body     = "{}"
 }
 

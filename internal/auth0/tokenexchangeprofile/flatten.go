383bb1a5a0ae13d76ac30e279ecf334559780d76
Rajat Bajaj <rajat.bajaj@okta.com>
2025-01-14 10:59:48 +0530

    Added docs and tests

--- internal/auth0/tokenexchangeprofile/flatten.go
+++ internal/auth0/tokenexchangeprofile/flatten.go
@@ -12,8 +12,8 @@ func flattenTokenExchangeProfile(data *schema.ResourceData, tokenExchangeProfile
 		data.Set("subject_token_type", tokenExchangeProfile.SubjectTokenType),
 		data.Set("action_id", tokenExchangeProfile.GetActionID()),
 		data.Set("type", tokenExchangeProfile.GetType()),
-		//data.Set("created_at", tokenExchangeProfile.GetCreatedAt()),
-		//data.Set("updated_at", tokenExchangeProfile.GetUpdatedAt()),
+		//Data.Set("created_at", tokenExchangeProfile.GetCreatedAt()),
+		//data.Set("updated_at", tokenExchangeProfile.GetUpdatedAt()),.
 	)
 	return result.ErrorOrNil()
 }
--- internal/auth0/tokenexchangeprofile/resource_test.go
+++ internal/auth0/tokenexchangeprofile/resource_test.go
@@ -9,41 +9,37 @@ import (
 	"github.com/auth0/terraform-provider-auth0/internal/acctest"
 )
 
-const testTokenExchangeProfileCreate = `
+const givenACustomTokenAction = `
 resource "auth0_action" "my_action" {
-	name = "Test Action {{.testName}}2"
-	code = "exports.onExecutePostLogin = async (event, api) => {};"
+	name = "{{.testName}}-Action"
+	code = <<-EOT
+		exports.onContinuePostLogin = async (event, api) => {
+			console.log("foo")
+		};"
+		EOT
 	deploy = true
 	supported_triggers {
 		id      = "custom-token-exchange"
 		version = "v1"
 	}
 }
+`
 
+const testTokenExchangeProfileCreate = givenACustomTokenAction + `
 resource "auth0_token_exchange_profile" "my_token_exchange_profile" {
-	name = "my-token-exc-profile-{{.testName}}"
+	name = "token-prof-{{.testName}}"
 	subject_token_type = "https://acme.com/cis-token"
 	action_id = auth0_action.my_action.id
 	type = "custom_authentication"
 }
 `
 
-const testTokenExchangeProfileUpdate = `
-resource "auth0_action" "my_action" {
-	name = "Test Action {{.testName}}2"
-	code = "exports.onExecutePostLogin = async (event, api) => {};"
-	deploy = true
-	supported_triggers {
-		id      = "custom-token-exchange"
-		version = "v1"
-	}
-}
-
+const testTokenExchangeProfileUpdate = givenACustomTokenAction + `
 resource "auth0_token_exchange_profile" "my_token_exchange_profile" {
-	name = "my-token-exc-profile-updated-{{.testName}}"
+	name = "token-prof-updated-{{.testName}}"
 	subject_token_type = "https://acme.com/cis-token-updated"
 	action_id = auth0_action.my_action.id
-	type = "custom_authentication_updated"
+	type = "custom_authentication"
 }
 `
 
@@ -53,7 +49,7 @@ func TestTokenExchangeProfile(t *testing.T) {
 			{
 				Config: acctest.ParseTestName(testTokenExchangeProfileCreate, t.Name()),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("auth0_token_exchange_profile.my_token_exchange_profile", "name", fmt.Sprintf("my-token-exc-profile-%s2", t.Name())),
+					resource.TestCheckResourceAttr("auth0_token_exchange_profile.my_token_exchange_profile", "name", fmt.Sprintf("token-prof-%s", t.Name())),
 					resource.TestCheckResourceAttr("auth0_token_exchange_profile.my_token_exchange_profile", "subject_token_type", "https://acme.com/cis-token"),
 					resource.TestCheckResourceAttr("auth0_token_exchange_profile.my_token_exchange_profile", "type", "custom_authentication"),
 					resource.TestCheckResourceAttrSet("auth0_token_exchange_profile.my_token_exchange_profile", "action_id"),
@@ -62,11 +58,13 @@ func TestTokenExchangeProfile(t *testing.T) {
 			{
 				Config: acctest.ParseTestName(testTokenExchangeProfileUpdate, t.Name()),
 				Check: resource.ComposeTestCheckFunc(
-					resource.TestCheckResourceAttr("auth0_token_exchange_profile.my_token_exchange_profile", "name", fmt.Sprintf("my-token-exc-profile-updated-%s2", t.Name())),
+					resource.TestCheckResourceAttr("auth0_token_exchange_profile.my_token_exchange_profile", "name", fmt.Sprintf("token-prof-updated-%s", t.Name())),
 					resource.TestCheckResourceAttr("auth0_token_exchange_profile.my_token_exchange_profile", "subject_token_type", "https://acme.com/cis-token-updated"),
-					resource.TestCheckResourceAttr("auth0_token_exchange_profile.my_token_exchange_profile", "type", "custom_authentication_updated"),
-					resource.TestCheckResourceAttrSet("auth0_token_exchange_profile.my_token_exchange_profile", "action_id"),
 				),
+			},
+			{
+				Config: acctest.ParseTestName(givenACustomTokenAction, t.Name()),
+				Check:  resource.ComposeTestCheckFunc(),
 			},
 		},
 	})

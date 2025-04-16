70ef37a8425d8237923625de41df034973b57a7f
kushalshit27 <43465488+kushalshit27@users.noreply.github.com>
2025-04-16 16:26:31 +0530

    Update Network ACL examples: Add match and not-match criteria with detailed descriptions

--- examples/resources/auth0_network_acl/resource.tf
+++ examples/resources/auth0_network_acl/resource.tf
@@ -1,6 +1,7 @@
-# Example:
-resource "auth0_network_acl" "my_network_acl" {
-  description = "My network ACL"
+# Example of auth0_network_acl with match criteria
+
+resource "auth0_network_acl" "my_network_acl_match" {
+  description = "Example with match network ACL"
   active      = true
   priority    = 1
   rule {
@@ -11,8 +12,25 @@ resource "auth0_network_acl" "my_network_acl" {
     match {
       geo_country_codes     = ["US", "CA"]
       geo_subdivision_codes = ["US-NY", "CA-ON"]
-      ipv4_cidrs            = ["192.168.1.0/24", "10.0.0.0/8"]
-      ipv6_cidrs            = ["2001:db8::/32"]
+
+    }
+  }
+}
+
+# Example of auth0_network_acl with not-match criteria
+resource "auth0_network_acl" "my_network_acl_not_match" {
+  description = "Example with not match network ACL"
+  active      = true
+  priority    = 3
+  rule {
+    action {
+      log = true
+    }
+    scope = "authentication"
+    not_match {
+      asns       = [9876]
+      ipv4_cidrs = ["192.168.1.0/24", "10.0.0.0/8"]
+      ipv6_cidrs = ["2001:db8::/32"]
     }
   }
 }

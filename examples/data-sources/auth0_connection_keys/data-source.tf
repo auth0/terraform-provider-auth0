4f62548eb72081731e3aecd3c1335ef5eae46c95
Rajat Bajaj <rajat.bajaj@okta.com>
2025-06-18 20:51:14 +0530

    Update docs

--- examples/data-sources/auth0_connection_keys/data-source.tf
+++ examples/data-sources/auth0_connection_keys/data-source.tf
@@ -1,4 +1,4 @@
 # An Auth0 Connection Keys loaded using its Connection ID.
 data "auth0_connection_keys" "my_keys" {
-    connection_id = "con_xxxxxxxxxxxxx"
+  connection_id = "con_xxxxxxxxxxxxx"
 }

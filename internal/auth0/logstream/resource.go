d86caa13ad0a13382bf91df6481c5410febca9e3
duedares-rvj <rajat.bajaj@okta.com>
2025-07-07 12:22:47 +0530

    fix:  make `filters` computed for auth0_log_stream (#1307)
    * Updated filters schema on auth0_log_stream
    
    * Updated logic for status update

--- internal/auth0/logstream/resource.go
+++ internal/auth0/logstream/resource.go
@@ -69,6 +69,7 @@ func NewResource() *schema.Resource {
 			"filters": {
 				Type:     schema.TypeList,
 				Optional: true,
+				Computed: true,
 				Description: "Only logs events matching these filters will be delivered by the stream." +
 					" If omitted or empty, all events will be delivered. " +
 					"Filters available: `auth.ancillary.fail`, `auth.ancillary.success`, `auth.login.fail`, " +
@@ -351,7 +352,9 @@ func createLogStream(ctx context.Context, data *schema.ResourceData, meta interf
 	// we perform an additional operation to modify it.
 	if status := data.Get("status").(string); status != "" && status != logStream.GetStatus() {
 		logStreamWithStatus := &management.LogStream{Status: &status}
-		return diag.FromErr(api.LogStream.Update(ctx, logStream.GetID(), logStreamWithStatus))
+		if err := api.LogStream.Update(ctx, logStream.GetID(), logStreamWithStatus); err != nil {
+			return diag.FromErr(internalError.HandleAPIError(data, err))
+		}
 	}
 
 	return readLogStream(ctx, data, meta)

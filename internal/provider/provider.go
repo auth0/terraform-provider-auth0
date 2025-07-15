70f79382e03a64581ffdb7876a6c9cec281d2476
Rajat Bajaj <rajat.bajaj@okta.com>
2025-07-15 15:55:07 +0530

    Bumped go-auth0 and some docs updated

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -124,7 +124,7 @@ func New() *schema.Provider {
 					}
 					return v == "1" || v == "true" || v == "on", nil
 				},
-				Description: "Indicates whether credentials will be dynamically passed to the provider from" +
+				Description: "Indicates whether credentials will be dynamically passed to the provider from " +
 					"other terraform resources.",
 			},
 			"cli_login": {

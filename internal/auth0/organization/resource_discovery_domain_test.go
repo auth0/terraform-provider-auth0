66118914fed86bb81e283a71940c4340a39e03e1
Ewan Harris <ewan.harris@okta.com>
2025-09-26 15:42:59 +0100

    feat: support auth0_organization_discovery_domain resource

---
+++ internal/auth0/organization/resource_discovery_domain_test.go
@@ -0,0 +1,103 @@
+package organization_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
+	"github.com/hashicorp/terraform-plugin-testing/terraform"
+
+	"github.com/auth0/terraform-provider-auth0/internal/acctest"
+)
+
+const testAccOrganizationDiscoveryDomainCreate = `
+resource "auth0_organization" "test_org" {
+	name         = "test-{{.testName}}"
+	display_name = "Test Organization {{.testName}}"
+}
+
+resource "auth0_organization_discovery_domain" "test_domain" {
+	organization_id = auth0_organization.test_org.id
+	domain         = "{{.testName}}.example.com"
+	status         = "pending"
+}
+`
+
+const testAccOrganizationDiscoveryDomainUpdate = `
+resource "auth0_organization" "test_org" {
+	name         = "test-{{.testName}}"
+	display_name = "Test Organization {{.testName}}"
+}
+
+resource "auth0_organization_discovery_domain" "test_domain" {
+	organization_id = auth0_organization.test_org.id
+	domain         = "{{.testName}}.example.com"
+	status         = "verified"
+}
+`
+
+func TestAccOrganizationDiscoveryDomain(t *testing.T) {
+	testName := strings.ToLower(t.Name())
+	domainName := strings.ReplaceAll(testName, "_", "-")
+
+	acctest.Test(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: strings.ReplaceAll(acctest.ParseTestName(testAccOrganizationDiscoveryDomainCreate, testName), testName+".example.com", domainName+".example.com"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_organization_discovery_domain.test_domain", "domain", fmt.Sprintf("%s.example.com", domainName)),
+					resource.TestCheckResourceAttr("auth0_organization_discovery_domain.test_domain", "status", "pending"),
+					resource.TestCheckResourceAttrSet("auth0_organization_discovery_domain.test_domain", "organization_id"),
+					resource.TestCheckResourceAttrSet("auth0_organization_discovery_domain.test_domain", "verification_txt"),
+					resource.TestCheckResourceAttrSet("auth0_organization_discovery_domain.test_domain", "verification_host"),
+				),
+			},
+			{
+				Config: strings.ReplaceAll(acctest.ParseTestName(testAccOrganizationDiscoveryDomainUpdate, testName), testName+".example.com", domainName+".example.com"),
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("auth0_organization_discovery_domain.test_domain", "domain", fmt.Sprintf("%s.example.com", domainName)),
+					resource.TestCheckResourceAttr("auth0_organization_discovery_domain.test_domain", "status", "verified"),
+					resource.TestCheckResourceAttrSet("auth0_organization_discovery_domain.test_domain", "organization_id"),
+					resource.TestCheckResourceAttrSet("auth0_organization_discovery_domain.test_domain", "verification_txt"),
+					resource.TestCheckResourceAttrSet("auth0_organization_discovery_domain.test_domain", "verification_host"),
+				),
+			},
+		},
+	})
+}
+
+const testAccOrganizationDiscoveryDomainImport = `
+resource "auth0_organization" "test_org" {
+	name         = "test-{{.testName}}"
+	display_name = "Test Organization {{.testName}}"
+}
+
+resource "auth0_organization_discovery_domain" "test_domain" {
+	organization_id = auth0_organization.test_org.id
+	domain         = "{{.testName}}.example.com"
+	status         = "pending"
+}
+`
+
+func TestAccOrganizationDiscoveryDomain_Import(t *testing.T) {
+	testName := strings.ToLower(t.Name())
+	domainName := strings.ReplaceAll(testName, "_", "-")
+
+	acctest.Test(t, resource.TestCase{
+		Steps: []resource.TestStep{
+			{
+				Config: strings.ReplaceAll(acctest.ParseTestName(testAccOrganizationDiscoveryDomainImport, testName), testName+".example.com", domainName+".example.com"),
+			},
+			{
+				ResourceName:      "auth0_organization_discovery_domain.test_domain",
+				ImportState:       true,
+				ImportStateVerify: true,
+				ImportStateIdFunc: func(state *terraform.State) (string, error) {
+					resourceState := state.RootModule().Resources["auth0_organization_discovery_domain.test_domain"]
+					return resourceState.Primary.ID, nil
+				},
+			},
+		},
+	})
+}

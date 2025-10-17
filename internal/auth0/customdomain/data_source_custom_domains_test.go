82b726cb9472b0627e771b41311aca6f52355e56
Rajat Bajaj <rajat.bajaj@okta.com>
2025-10-17 14:17:24 +0530

    updated test cases

--- internal/auth0/customdomain/data_source_custom_domains_test.go
+++ internal/auth0/customdomain/data_source_custom_domains_test.go
@@ -8,34 +8,34 @@ import (
 	"github.com/auth0/terraform-provider-auth0/internal/acctest"
 )
 
-const testAccCustomDomain1 = `
+const testAccCustomDomainFirst = `
 resource "auth0_custom_domain" "my_custom_domain1" {
   domain = "authninja1.auth.tempdomain.com"
   type   = "self_managed_certs"
 }
 `
 
-const testAccCustomDomain2 = `
+const testAccCustomDomainSecond = testAccCustomDomainFirst + `
 resource "auth0_custom_domain" "my_custom_domain2" {
   domain = "authninja2.auth.tempdomain.com"
   type   = "self_managed_certs"
 }
 `
 
-const testAccCustomDomain3 = `
+const testAccCustomDomainThird = testAccCustomDomainSecond + `
 resource "auth0_custom_domain" "my_custom_domain3" {
   domain = "beacon.auth.tempdomain.com"
   type   = "self_managed_certs"
 }
 `
 
-const testAccDataSourceCustomDomainsFilter1 = testAccCustomDomain1 + testAccCustomDomain2 + testAccCustomDomain3 + `
+const testAccDataSourceCustomDomainsFilter1 = `
 data "auth0_custom_domains" "filtered" {
   q = "domain:authninja*"
 }
 `
 
-const testAccDataSourceCustomDomainsFilter2 = testAccCustomDomain1 + testAccCustomDomain2 + testAccCustomDomain3 + `
+const testAccDataSourceCustomDomainsFilter2 = `
 data "auth0_custom_domains" "filtered" {
   q = "domain:beacon*"
 }
@@ -45,10 +45,16 @@ func TestAccDataSourceCustomDomains(t *testing.T) {
 	acctest.Test(t, resource.TestCase{
 		Steps: []resource.TestStep{
 			{
-				Config: testAccCustomDomain1 + testAccCustomDomain2 + testAccCustomDomain3,
+				// We had to split this into two separate posts to work around an issue
+				// in the test recording library. We need to add X-Request-Id header to the POST requests
+				// to fix this, and make sure that go-vcr uses that to match requests.
+				Config: acctest.ParseTestName(testAccCustomDomainFirst, t.Name()),
 			},
 			{
-				Config: testAccDataSourceCustomDomainsFilter1,
+				Config: acctest.ParseTestName(testAccCustomDomainSecond, t.Name()),
+			},
+			{
+				Config: testAccCustomDomainThird + testAccDataSourceCustomDomainsFilter1,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.auth0_custom_domains.filtered", "q", "domain:authninja*"),
 					resource.TestCheckResourceAttr("data.auth0_custom_domains.filtered", "custom_domains.#", "2"),
@@ -60,7 +66,7 @@ func TestAccDataSourceCustomDomains(t *testing.T) {
 				),
 			},
 			{
-				Config: testAccDataSourceCustomDomainsFilter2,
+				Config: testAccCustomDomainThird + testAccDataSourceCustomDomainsFilter2,
 				Check: resource.ComposeTestCheckFunc(
 					resource.TestCheckResourceAttr("data.auth0_custom_domains.filtered", "q", "domain:beacon*"),
 					resource.TestCheckResourceAttr("data.auth0_custom_domains.filtered", "custom_domains.#", "1"),

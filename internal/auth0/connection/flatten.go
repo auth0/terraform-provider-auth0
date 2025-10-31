fb118f7fb3d309c28e53bd7ce0f5ae928227a236
duedares-rvj <rajat.bajaj@okta.com>
2025-10-31 10:37:35 +0530

    Feat: Add support for managing OAuth1 Connections (#1394)
    * Added expand/flatten for oauth1 connection types
    
    # Conflicts:
    #	go.mod
    #	go.sum
    
    * Updated schema
    
    * Updated docs
    
    * docs update

--- internal/auth0/connection/flatten.go
+++ internal/auth0/connection/flatten.go
@@ -54,6 +54,8 @@ var flattenConnectionOptionsMap = map[string]flattenConnectionOptionsFunc{
 	management.ConnectionStrategySAML:         flattenConnectionOptionsSAML,
 	management.ConnectionStrategyADFS:         flattenConnectionOptionsADFS,
 	management.ConnectionStrategyPingFederate: flattenConnectionOptionsPingFederate,
+
+	management.ConnectionStrategyOAuth1: flattenConnectionOptionsOAuth1,
 }
 
 type flattenConnectionOptionsFunc func(data *schema.ResourceData, options interface{}) (interface{}, diag.Diagnostics)
@@ -1142,6 +1144,30 @@ func flattenConnectionOptionsPingFederate(
 
 	if options.GetSetUserAttributes() == "" {
 		optionsMap["set_user_root_attributes"] = "on_each_login"
+	}
+
+	return optionsMap, nil
+}
+
+func flattenConnectionOptionsOAuth1(
+	_ *schema.ResourceData,
+	rawOptions interface{},
+) (interface{}, diag.Diagnostics) {
+	options, ok := rawOptions.(*management.ConnectionOptionsOAuth1)
+	if !ok {
+		return nil, diag.FromErr(errUnsupportedConnectionOptionsType)
+	}
+
+	optionsMap := map[string]interface{}{
+		"consumer_key":           options.GetConsumerKey(),
+		"consumer_secret":        options.GetConsumerSecret(),
+		"request_token_url":      options.GetRequestTokenURL(),
+		"access_token_url":       options.GetAccessTokenURL(),
+		"user_authorization_url": options.GetUserAuthorizationURL(),
+		"session_key":            options.GetSessionKey(),
+		"signature_method":       options.GetSignatureMethod(),
+		"custom_headers":         flattenCustomHeaders(options.GetCustomHeaders()),
+		"scripts":                options.GetScripts(),
 	}
 
 	return optionsMap, nil

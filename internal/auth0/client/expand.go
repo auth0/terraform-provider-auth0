15b57e121a596d2474ec2c884fef3ac054e09bea
Kiran Kumar <kiran.kumar@okta.com>
2026-01-09 14:49:42 +0530

    fix: Add null check for organization discovery methods in fetchNullableFields

--- internal/auth0/client/expand.go
+++ internal/auth0/client/expand.go
@@ -1090,6 +1090,7 @@ func fetchNullableFields(data *schema.ResourceData, client *management.Client) m
 		},
 		"token_quota": commons.IsTokenQuotaNull,
 		"skip_non_verifiable_callback_uri_confirmation_prompt": isSkipNonVerifiableCallbackURIConfirmationPromptNull,
+		"organization_discovery_methods": isOrganizationDiscoveryMethodsNull,
 	}
 
 	nullableMap := make(map[string]interface{})
@@ -1204,6 +1205,20 @@ func isSkipNonVerifiableCallbackURIConfirmationPromptNull(data *schema.ResourceD
 	}
 
 	return rawConfig.GetAttr("skip_non_verifiable_callback_uri_confirmation_prompt").IsNull()
+}
+
+func isOrganizationDiscoveryMethodsNull(data *schema.ResourceData) bool {
+	if !data.IsNewResource() && !data.HasChange("organization_discovery_methods") {
+		return false
+	}
+
+	rawConfig := data.GetRawConfig()
+	if rawConfig.IsNull() {
+		return true
+	}
+
+	attr := rawConfig.GetAttr("organization_discovery_methods")
+	return attr.IsNull()
 }
 
 func expandExpressConfiguration(data *schema.ResourceData) *management.ExpressConfiguration {

4d534a2b97dbace6830d8c2342bc05bce75102b0
KIRAN KUMAR B <kiran.kumar@okta.com>
2026-02-16 22:35:47 +0530

    Add useOauthSpecScope option to OAuth2 connection (#1480)
    feat(connection): add useOauthSpecScope option to OAuth2 connection
    
    - Introduced `use_oauth_spec_scope` to the OAuth2 connection options.
    - This boolean option determines the format of `scopes` as per OAuth2 specification.
    - Updated `expandConnectionOptionsOAuth2`, `flattenConnectionOptionsOAuth2`, and the schema definition to accommodate this new option.
    - Updated test case to validate the new option in `TestAccConnectionOAuth2`.

--- internal/auth0/connection/flatten.go
+++ internal/auth0/connection/flatten.go
@@ -589,6 +589,7 @@ func flattenConnectionOptionsOAuth2(
 		"pkce_enabled":             options.GetPKCEEnabled(),
 		"strategy_version":         options.GetStrategyVersion(),
 		"email":                    options.GetEmail(),
+		"use_oauth_spec_scope":     options.GetUseOauthSpecScope(),
 		"upstream_params":          upstreamParams,
 		"custom_headers":           flattenCustomHeaders(options.GetCustomHeaders()),
 	}
